fa8a4c08d5d7cf84275d909323449ecc9ca4ec32
Ameya Chikodi <achikodi@clearblade.com>
2016-06-23 13:51:02 -0500

    system secret requirement removed

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ function SignIn(event){
     var activeKey = document.getElementById("activeKey").value;
     var platformUrl = document.getElementById("platformUrl").value;
     var systemKey = document.getElementById("systemKey").value;
-    var systemSecret = document.getElementById("systemSecret").value;
+    //var systemSecret = document.getElementById("systemSecret").value;
     var signInData = {"deviceName":deviceName, "activeKey":activeKey};
     console.log(JSON.stringify(signInData));
     var client;
@@ -42,7 +42,7 @@ function SignIn(event){
     client.open("POST",url_action,true);
     client.setRequestHeader("Content-type", "application/json");
     client.setRequestHeader("ClearBlade-Systemkey", systemKey);
-    client.setRequestHeader("ClearBlade-Systemsecret", systemSecret);
+    //client.setRequestHeader("ClearBlade-Systemsecret", systemSecret);
     //client.setRequestHeader("Clearblade-usertoken","5kZ6YFTLqb1EmLUjVr-MPl8S125pXE-gNfC1OX8Pv6860IzLsQZsVoBwLbqz6oH5isT0ebXkcP9pBVM1")
     //curl -X POST https://staging.clearblade.com/api/v/2/devices/f887dcf20ac88ade9da9c193facd01/auth -d '{"deviceName":"fan", "deviceToken":"AmeyaChikodi"}' -H 'ClearBlade-Systemkey:f887dcf20ac88ade9da9c193facd01' -H 'ClearBlade-Systemsecret:F887DCF20AFEE5908181DFC4FEB701' -H 'Clearblade-usertoken:5kZ6YFTLqb1EmLUjVr-MPl8S125pXE-gNfC1OX8Pv6860IzLsQZsVoBwLbqz6oH5isT0ebXkcP9pBVM1'
     //curl -X POST https://rtp.clearblade.com/api/v/2/devices/acd4cbf60a9eadefb5b5bdcee7cd01/auth -d '{"deviceName":"fan", "activeKey":"AmeyaChikodi"}' -H 'ClearBlade-Systemkey:acd4cbf60a9eadefb5b5bdcee7cd01' -H 'ClearBlade-Systemsecret:ACD4CBF60AF2F8F7B294C7CDE274'
